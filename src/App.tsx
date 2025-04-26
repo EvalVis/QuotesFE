ec6cafd6de81849890ed0e4eb55834119dcd7308
Evaldas Visockas <vievaldas@gmail.com>
2025-04-26 21:09:26 +0300

    Display random quotes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,33 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 
+interface Quote {
+  quote: string;
+  author: string;
+  tags: string[];
+}
+
 function App() {
-  const [response, setResponse] = useState('')
+  const [quotes, setQuotes] = useState<Quote[]>([])
 
   useEffect(() => {
-    fetch('https://quotesapi.fly.dev')
-      .then(res => res.text())
-      .then(data => setResponse(data))
+    fetch('https://quotesapi.fly.dev/api/quotes/random')
+      .then(res => res.json())
+      .then(data => setQuotes(data))
   }, [])
 
   return (
     <div>
       <h1>Quotes API</h1>
-      <p>{response}</p>
+      <hr />
+      {quotes.map((quote, index) => (
+        <div key={index}>
+          <p>"{quote.quote}"</p>
+          <p>- {quote.author}</p>
+          <p>{quote.tags.join(', ')}</p>
+          <hr />
+        </div>
+      ))}
     </div>
   )
 }
