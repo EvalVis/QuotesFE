3667a87b3c969f5637698a048f682cc2084c74ba
Evaldas Visockas <vievaldas@gmail.com>
2025-04-28 20:50:12 +0300

    Change callback address

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ createRoot(document.getElementById('root')!).render(
       domain="dev-wzfkg4o26oz6ndmt.us.auth0.com"
       clientId="v0YzxpAoJP6tLyW29TnZEuqStYkUF5fY"
       authorizationParams={{
-        redirect_uri: window.location.origin,
+        redirect_uri: `${window.location.origin}/callback`,
         audience: "quotes.programmersdiary.com"
       }}
       cacheLocation="localstorage"
