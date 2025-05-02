98a429cb63e2793ed684a597c60147fde2bc1767
Evaldas Visockas <vievaldas@gmail.com>
2025-05-02 20:03:22 +0300

    Add coverage check

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './setupTests.ts'
+    setupFiles: './setupTests.ts',
+    coverage: {
+      reporter: ['text', 'lcov']
+    },
   },
 });
